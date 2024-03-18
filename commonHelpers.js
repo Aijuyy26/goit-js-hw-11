959c6d15ad74db362f744153ce397dff4dd75ca3
Aijuyy26 <146477985+Aijuyy26@users.noreply.github.com>
2024-03-18 14:12:47 +0000

    Deploying to gh-pages from @ Aijuyy26/goit-js-hw-11@5023fd56c56c14cf4991fa2c38db4dff5db71836 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as u,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function n(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function s(t){if(t.ep)return;t.ep=!0;const e=n(t);fetch(t.href,e)}})();function h(r){return r=encodeURIComponent(r),i.innerHTML='<div class="loader"></div>',fetch(`https://pixabay.com/api/?key=42609290-856768105ab9e79485c69bf61&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}function p(r){i.innerHTML="",console.log(r);const o=r.hits;o.length==0&&u.error({title:"Error",message:"Error: No such pictures!",position:"topRight"});const n=o.map(e=>`<li class="gallery-item">
+import{i as u,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();function h(t){return t=encodeURIComponent(t),i.innerHTML='<div class="loader"></div>',fetch(`https://pixabay.com/api/?key=42609290-856768105ab9e79485c69bf61&q=${t}&image_type=photo&orientation=horizontal&safesearch=true`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}function p(t){const o=t.hits;if(o.length===0){u.error({title:"Error",message:"Error: No such pictures!",position:"topRight"});return}const n=document.createElement("div");n.classList.add("loader"),i.innerHTML="",i.appendChild(n);const s=o.map(e=>`<li class="gallery-item">
         <a class="gallery-link" href="${e.largeImageURL}">
           <img
             class="gallery-image"
@@ -9,15 +9,11 @@ import{i as u,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=docum
             alt="${e.tags}"
           />
           <ul class="gallery-description">
-          <li><h3>Likes</h3><p>${e.likes}</p>
-          </li>
-          <li><h3>Views</h3><p>${e.views}</p>
-            </li>
-            <li><h3>Comments</h3><p>${e.comments}</p>
-              </li>
-              <li><h3>Downloads</h3><p>${e.downloads}</p>
-                </li>
+            <li><h3>Likes</h3><p>${e.likes}</p></li>
+            <li><h3>Views</h3><p>${e.views}</p></li>
+            <li><h3>Comments</h3><p>${e.comments}</p></li>
+            <li><h3>Downloads</h3><p>${e.downloads}</p></li>
           </ul>
         </a>
-      </li>`).join("");i.insertAdjacentHTML("beforeend",n);const s={captionsData:"alt"};let t=new d(".gallery a",s);t.on("show.simplelightbox",function(){}),t.refresh()}const i=document.querySelector("ul.gallery");let c="";const a=document.getElementById("search-input");a.addEventListener("input",r=>{c=a.value.trim(),i.innerHTML=""});const f=document.getElementById("search-button");f.addEventListener("click",()=>{c&&(i.innerHTML='<div class="loader"></div>',h(c).then(r=>p(r)).catch(r=>{console.log(r),u.error({title:"Error",message:"Виникла помилка під час завантаження зображень. Будь ласка, спробуйте пізніше.",position:"topRight"})}))});
+      </li>`).join("");setTimeout(()=>{i.innerHTML=s,new d(".gallery a").on("show.simplelightbox",function(){})},1e3)}const i=document.querySelector("ul.gallery");let c="";const a=document.getElementById("search-input");a.addEventListener("input",t=>{c=a.value.trim(),i.innerHTML=""});const f=document.getElementById("search-button");f.addEventListener("click",()=>{c&&(i.innerHTML='<div class="loader"></div>',h(c).then(t=>p(t)).catch(t=>{console.log(t),u.error({title:"Error",message:"Виникла помилка під час завантаження зображень. Будь ласка, спробуйте пізніше.",position:"topRight"})}))});
 //# sourceMappingURL=commonHelpers.js.map
